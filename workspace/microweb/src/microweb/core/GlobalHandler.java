6409d883caef02c574a2a3d4ef8b57e80ea70a40
Shaha Alam <shaha.alam@infomentum.co.uk>
2018-02-20 18:33:52 +0000

    no message

--- workspace/microweb/src/microweb/core/GlobalHandler.java
+++ workspace/microweb/src/microweb/core/GlobalHandler.java
@@ -26,9 +26,7 @@ public class GlobalHandler implements Filter {
      * Default constructor. 
      */
     public GlobalHandler() {
-    	String loggerName = this.getClass().getPackage().getName();
-    	System.out.println("Logging to " + loggerName);
-    	this.logger = Logger.getLogger(loggerName);
+    	this.logger = Logger.getLogger(this.getClass().getPackage().getName());
     }
 
 	/**
@@ -44,30 +42,16 @@ public class GlobalHandler implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest) request;
 		String path = req.getRequestURI();
-		System.out.println("path: " + path);
+		this.logger.finest("path: " + path);
 		
-	    String topfolder = path.substring(1);
-	    System.out.println("topfolder1: " + topfolder);
-	    
-	    if (topfolder.contains("/")) {
-	        topfolder = topfolder.substring(0, topfolder.indexOf("/"));
-	    }
-	    
-	    System.out.println("topfolder2: " + topfolder);
-
-	    
-	    this.logger.severe("[" + Level.SEVERE + "] SEVERE message");
-	    this.logger.warning("[" + Level.WARNING + "] WARNING message");
-	    this.logger.info("[" + Level.INFO  + "] INFO  message");
-	    this.logger.config("[" + Level.CONFIG + "] CONFIG message");
-	    this.logger.fine("[" + Level.FINE + "] FINE message");
-	    this.logger.finer("[" + Level.FINER + "] FINER message");
-	    this.logger.finest("[" + Level.FINEST + "] FINEST message");
-	    
-	    
-	    if (topfolder.startsWith("css")) {
+	   
+	    if (path.startsWith("/site/css")) {
+	    	//forward on statuc css urls
+	    	this.logger.finest("getting static resource");
 	        chain.doFilter(request, response);
 	    } else {
+	    	//handle all other urls with servlet
+	    	this.logger.finest("redirecting to controller");
 	        request.getRequestDispatcher("/Controller" + path).forward(request, response);
 	    }
 	}

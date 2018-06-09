c6463ea2deab6a2eedd18f0f6e8d7aed424688d4
Shaha Alam <shaha.alam@infomentum.co.uk>
2018-06-09 15:41:33 +0100

    it now stores pagehandlers so that they dont have to be recreated over and over.

--- workspace/microweb/src/microweb/impl/CanonicalDomain.java
+++ workspace/microweb/src/microweb/impl/CanonicalDomain.java
@@ -2,6 +2,7 @@ package microweb.impl;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,44 +16,82 @@ import microweb.model.PageHandler;
 
 public class CanonicalDomain extends AbstractDomain {
 
+	private Map<String, PageHandler> pageHandlerRegistry;
+	
 	protected CanonicalDomain(String name, String site) throws XPathExpressionException {
 		super(name, site);
 		
+		this.pageHandlerRegistry = new ConcurrentHashMap<String, PageHandler>();
+		
 		if (this.getSite() != null) {
-			Domain domain = Util.getSiteCanonicalDomainsRegistry().get(this.getSite().getName());
 			
-			if (domain != null) {
-				//site already has a canonical domain
-				logger.log(Level.WARNING, "microweb.application.config.sites-config.multipleCanonicalDomains", new Object[] {this.getSite().getName(), domain.getName(), this.getName()});
+			Domain existingDomain = Util.getSiteCanonicalDomainsRegistry().putIfAbsent(name, this);
+			
+			if (existingDomain != null) {
+				if(logger.isLoggable(Level.WARNING)) {
+					logger.log(Level.WARNING, "microweb.application.config.sites-config.multipleCanonicalDomains", new Object[] {this.getSite().getName(), existingDomain.getName(), this.getName()});
+				}
 			} else {
-				Util.getSiteCanonicalDomainsRegistry().put(this.getSite().getName(), this);
+				if(logger.isLoggable(Level.FINE)) {
+					logger.fine("Registered canonical domain: " + this.getName() + " for site: " +  this.getSite().getName());
+				}
 			}
+
+		} else {
+			throw new RuntimeException("Could not find associated site for this domain: " + this.getName());
 		}
 		
 	}
 	
 	@Override
 	public void handle(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		if (logger.isLoggable(Level.FINEST)) {
-			logger.finest(this.getClass().getCanonicalName() + " is passing on to site handler for site: " + this.getSite().getName());
-		}
 		
 		String uri = request.getRequestURI();
 		
-		
-		//Map<String, PageHandler> pageRegistry = this.getSite().getPageRegistry();
-		
-		PageHandler pageHandler = site.getPageHandler(uri);
+		PageHandler pageHandler = pageHandlerRegistry.get(uri);
 		
 		if (pageHandler == null) {
-			response.sendError(HttpServletResponse.SC_NOT_FOUND);
-		} else {
+			//Map<String, PageHandler> pageRegistry = this.getSite().getPageRegistry();
 			if (logger.isLoggable(Level.FINEST)) {
-				logger.finest(this.getClass().getCanonicalName() + " is passing on to page handler: " + pageHandler.toString() + " for site: " + this.getSite());
+				logger.finest("Did not find existing Page Handler for domain: " + this.getName() + " and uri: " + uri + ".  Requesting a new Page Handler from site: " + this.getSite().getName());
 			}
 			
-			pageHandler.handle(request, response);
+			pageHandler = site.getPageHandler(uri);
+			
+			if (pageHandler == null) {
+				
+				if (logger.isLoggable(Level.FINEST)) {
+					logger.finest("Site: " + this.getSite().getName() + " did not return a Page handler for the domain: " + this.getName() + " and uri: " + uri + ", sending Page Not Found.");
+				}
+				
+				response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			} else {
+				
+				PageHandler existingPageHandler = pageHandlerRegistry.putIfAbsent(uri, pageHandler);
+				
+				if (existingPageHandler != null) {
+					if (logger.isLoggable(Level.FINEST)) {
+						logger.finest("Attempted to add a new PageHandler for the uri: " + uri + " to the domain: " + this.getName() + " but found that one already exists.  discarding this one after use.");
+					}
+				}
+				
+				this.forwardTo(pageHandler, request, response);
+			}
+		} else {
+			
+			if (logger.isLoggable(Level.FINEST)) {
+				logger.finest("Found existing Page Handler in the Page Handler Registry for the " + this.getName() + " domain");
+			}
+			
+			this.forwardTo(pageHandler, request, response);
 		}
 		
+		
+		
+		
+	}
+	
+	protected void forwardTo(PageHandler pageHandler, HttpServletRequest request, HttpServletResponse response) throws IOException {
+		pageHandler.handle(request, response);
 	}
 }
